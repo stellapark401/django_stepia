0b856dff1eb1481b6c3d7355ffaa85c871831b4a
stellapark401 <stellapark401@gmail.com>
2021-05-28 19:30:25 +0900

    2021.05.28

---
+++ twitter_automation/sectets.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
+
+
+def get_credentials():
+    credentials = {}
+    with open('credentials.txt') as fh:
+        for line in fh.readlines():
+            try:
+                k, v = line.split(': ')
+            except ValueError:
+                print('fill out the credential in the file.')
+                exit(0)
+            credentials[k] = v.rstrip(' \n')
+    return credentials
+
